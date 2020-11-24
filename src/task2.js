b3a58a518c5e42744486dce68c6fce44cff1eda7
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-11-24 14:02:58 +0300

    Processing csv line-by-line

--- src/task2.js
+++ src/task2.js
@@ -1,42 +1,6 @@
-/*
-The solution with input fully loaded to memory
-
-const { writeFile } = require('fs');
-const csv = require('csvtojson');
-const csvSource = ".csv/source.csv";
-const csvTarget = ".csv/target.csv";
-
-const uncapitalizeKeys = obj => {
-  return Object.fromEntries(Object.entries(obj)
-      .map(([k, v]) => [k.toLowerCase(), v]));
-};
-
-const formatEntries = entries => entries.map(entry => {
-  delete entry["Amount"];
-  return uncapitalizeKeys(entry);
-});
-
-const stringifyOutput = entries => {
-  return entries.map(entry => JSON.stringify(entry))
-    .join("\n");
-};
-
-const writeToFile = content => {
-  writeFile(csvTarget, content, err => {
-    if (err) return console.log(err);
-  });
-};
-
-csv()
-  .fromFile(csvSource)
-  .then(formatEntries)
-  .then(stringifyOutput)
-  .then(writeToFile);
-
-*/
-
-import readline from "readline";
 import { createReadStream, createWriteStream, unlinkSync, existsSync } from "fs";
+import csv from "csvtojson";
+import { Transform } from "stream";
 
 const csvSource = ".csv/source.csv";
 const csvTarget = ".csv/target.csv";
@@ -51,50 +15,33 @@ const init = () => {
   }
 }
 
-const composeJsonString = text => {
-  const values = text.split(",");
-  const obj = {
-    book: values[0],
-    author: values[1],
-    price: values[3]
-  };
-  return `${JSON.stringify(obj)}\n`;
-};
+const transformer = new Transform({objectMode:true});
 
-const writeLine = (line) => {
-  const data = composeJsonString(line);
-  const writeStream = createWriteStream(csvTarget, {flags: 'a'});
-  writeStream.once('open', () => {
-    writeStream.write(data);
-    writeStream.end();
-  });
-  writeStream.on('error', function(error) {
-    console.error(error);
-    writeStream.end();
-});
-};
-
-const openReadStream = (path) => {
-  const stream = createReadStream(path);
-  stream.on('error', function(error) {
-    console.error(error);
-  });
-  return stream;
-};
-
-async function processLineByLine() {
-  const fileStream = openReadStream(csvSource);
-  const lines = readline.createInterface({input: fileStream});
-  let firstRun = true;
-
-  for await (const line of lines) {
-    if (firstRun === true) {
-      firstRun = false;
-      continue;
-    } 
-    writeLine(line);
-  }
+transformer._transform = function(data, encoding, done){
+    const entry = JSON.parse(data.toString());
+    const obj = {
+      book: entry.Book,
+      author: entry.Author,
+      price: entry.Price
+    };
+    this.push(`${JSON.stringify(obj)}\n`);
+    done();
 }
 
+const errorHandler = e => {
+  console.error(e);
+};
+
+const execute = () => {
+  createReadStream(csvSource)
+    .on('error', errorHandler)
+    .pipe(csv())
+    .on('error', errorHandler)
+    .pipe(transformer)
+    .on('error', errorHandler)
+    .pipe(createWriteStream(csvTarget))
+    .on('error', errorHandler);
+};
+
 init();
-processLineByLine();
+execute();
