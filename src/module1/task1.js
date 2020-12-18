b865f8865ee6716c0cb46f2f841e900995f08846
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-12-18 11:38:03 +0300

    Restructure the project

--- src/module1/task1.js
+++
@@ -1,8 +0,0 @@
-const stdin = process.openStdin();
-
-const normalizeString = data => data.toString().trim();
-const reverse = text => text.split("").reverse().join("");
-const printReverse = input => console.log(`${reverse(normalizeString(input))}\n`);
-
-stdin.addListener("data", printReverse);
-console.log("Awaiting for input...");
