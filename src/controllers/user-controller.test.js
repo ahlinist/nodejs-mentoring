769c319e5ed5825b9f0fb3f39aaba3cb5ddd2cb9
Anton Hlinisty <anton.hlinisty@gmail.com>
2021-03-02 11:22:59 +0300

    Add unit tests

---
+++ src/controllers/user-controller.test.js
@@ -0,0 +1,50 @@
+const { get } = require('../services/user-service.js');
+
+jest.mock('../services/user-service.js', () => {
+  return {
+    get: jest.fn()
+  }
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+test('should respond with user object', async () => {
+    const user = {name: "asd"};
+    const id = 1;
+    const mockSend = jest.fn();
+    const mockRequest = {params: {id: id}};
+    const mockResponse = {send: mockSend};
+    const mockNext = jest.fn();
+
+    get.mockReturnValueOnce(user)
+
+    await require('./user-controller.js').get(mockRequest, mockResponse, mockNext);
+
+    expect(get).toHaveBeenCalledTimes(1);
+    expect(get).toHaveBeenCalledWith(id);
+    expect(mockSend).toHaveBeenCalledTimes(1);
+    expect(mockSend).toHaveBeenCalledWith(user);
+    expect(mockNext).toHaveBeenCalledTimes(1);
+});
+
+test('should return 404 as of missing user', async () => {
+  const id = 1;
+  const mockSend = jest.fn();
+  const mockStatus = jest.fn();
+  const mockRequest = {params: {id: id}};
+  const mockResponse = {send: mockSend, status: mockStatus};
+  const mockNext = jest.fn();
+
+  mockStatus.mockReturnValueOnce(mockResponse);
+
+  await require('./user-controller.js').get(mockRequest, mockResponse, mockNext);
+
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith(id);
+  expect(mockStatus).toHaveBeenCalledTimes(1);
+  expect(mockStatus).toHaveBeenCalledWith(404);
+  expect(mockSend).toHaveBeenCalledTimes(1);
+  expect(mockSend).toHaveBeenCalledWith("No user found with id 1");
+});
